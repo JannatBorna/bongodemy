d8b2215080dcd7f5cef88f9056e1fafcb074b4cb
alamin <ma7029582@gmail.com>
2022-01-20 11:05:15 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  
 } from "react-router-dom";
 import Navigation from './Component/Navigation';
 import Home from './Component/Home';
