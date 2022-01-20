15fb3d11d49e5adcac0d57833d8ca2634e771657
fatehahmod <ahmod.fateh@gmail.com>
2022-01-20 23:14:13 +0600

    added about us page

--- src/Component/pages/Home/Home.js
+++ src/Component/pages/Home/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Cards from '../../subComp/Cards';
+// import Cards from '../../subComp/Cards';
 import './Home.css'
 
 const Home = () => {
     return (
             <div className="container">
-                <div className="row mt-5">
+                {/* <div className="row mt-5">
                     <Cards text="Cyberbullying" follow ="#"></Cards>
                     <Cards text="Cyber crime" follow ="#"></Cards>
 
@@ -13,7 +13,7 @@ const Home = () => {
                 <div className="row my-5">
                     <Cards text="Cyber Security Solution" follow ="#"></Cards>
                     <Cards text="Cyber Security Training" follow ="#"></Cards>
-                </div>
+                </div> */}
             </div>
     );
 };
