6c77329667467497012169a7bf05d892925a1ed8
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-01-21 21:07:44 +0600

    add

--- src/Component/pages/Home/Home.js
+++ src/Component/pages/Home/Home.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import Cards from '../../subComp/Cards';
-
+import Cards from './../../subComp/Cards';
 
 const Home = () => {
     return (
-            <div className="container">
-                <div className="row mt-5">
-                    <Cards text="Cyberbullying" follow ="#"></Cards>
-                    <Cards text="Cyber crime" follow ="#"></Cards>
+        <div className="container">
+            <div className="row mt-5">
+                <Cards text="Cyberbullying" follow="#"></Cards>
+                <Cards text="Cyber crime" follow="#"></Cards>
 
-                </div>
-                <div className="row my-5">
-                    <Cards text="Cyber Security Solution" follow ="#"></Cards>
-                    <Cards text="Cyber Security Training" follow ="#"></Cards>
-                </div>
             </div>
+            <div className="row my-5">
+                <Cards text="Cyber Security Solution" follow="#"></Cards>
+                <Cards text="Cyber Security Training" follow="#"></Cards>
+            </div>
+        </div>
     );
 };
 
