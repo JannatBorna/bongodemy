319e9903e18d31b2b8b56da8215a24f58253da45
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-01-24 23:58:19 +0600

    add

--- src/Component/Shared/Header/Navigation.js
+++ src/Component/Shared/Header/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navigation.css'
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Dropdown } from 'react-bootstrap';
 import {
 
     Link
@@ -10,33 +10,45 @@ const Navigation = () => {
 
         <Navbar className='navbarstyle'  expand="lg ">
             <Container fluid>
-                <Navbar.Brand className='text-black' href="#">BongoDemy</Navbar.Brand>
+                <Navbar.Brand className='text-white' href="#">BongoDemy</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className=" text-decoration-none text-black  d-flex justify-content-around align-items-center "
+                    <Nav className=" text-decoration-none d-flex justify-content-around align-items-center ">
 
-                    >
-                            <Link className='text-black ms-4 text-decoration-none' to='/home'>Home</Link>
-                            {/* <Link className='text-black ms-4 text-decoration-none' to='/cyberBulling'>Cyberbullying</Link>
-                            <Link className='text-black ms-4 text-decoration-none' to='/cyberCrime'>Cybercrime</Link> */}
-                            <Link className='text-black ms-4 text-decoration-none' to='/services'>Services</Link>
-                            {/* <Link className='text-black ms-4 text-decoration-none' to='/training'>Training</Link> */}
+                            <Link className='text-white ms-4 text-decoration-none' to='/home'>Home</Link>
+                            {/* <Link className='text-white ms-4 text-decoration-none' to='/cyberBulling'>Cyberbullying</Link>
+                            <Link className='text-white ms-4 text-decoration-none' to='/cyberCrime'>Cybercrime</Link> */}
+                            <Link className='text-white ms-4 text-decoration-none' to='/services'>Services</Link>
+                            {/* <Link className='text-white ms-4 text-decoration-none' to='/training'>Training</Link> */}
 
-                       
-                        <NavDropdown title="Training" className='dropdown_color ms-4 text-decoration-none' id="basic-nav-dropdown">
+
+                        {/* <NavDropdown title="Training" className='dropdown_color text-white ms-4 text-decoration-none' id="basic-nav-dropdown">
                             <div className='dropdown_item'>
                                 <NavDropdown.Item href="/freeTraining" className='text-white dropdown_text'>Free Training</NavDropdown.Item>
                                 <NavDropdown.Item href="/courses" className='text-white dropdown_text'>Courses</NavDropdown.Item>
                                 <NavDropdown.Item href="/hackingLab" className='text-white dropdown_text'>Hacking Lab</NavDropdown.Item>
                                 <NavDropdown.Item href="/battleGround" className='text-white dropdown_text'>Battle Ground</NavDropdown.Item> 
                             </div>
-                            </NavDropdown>
+                            </NavDropdown> */}
+
+                        <Dropdown>
+                            <Dropdown.Toggle id="dropdown-basic" className='mx-4'>
+                                Training
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu className='dropdown_item'>
+                                <Dropdown.Item href="/freeTraining" className='text-white dropdown_text'>Free Training</Dropdown.Item>
+                                <Dropdown.Item href="/courses" className='text-white dropdown_text'>Courses</Dropdown.Item>
+                                <Dropdown.Item href="/hackingLab" className='text-white dropdown_text'>Hacking Lab</Dropdown.Item>
+                                <Dropdown.Item href="/battleGround" className='text-white dropdown_text'>Battle Ground</Dropdown.Item>
+                            </Dropdown.Menu>
+                           
+                        </Dropdown>
                        
 
-                            <Link className='text-black ms-4 text-decoration-none' to='/resources'>Resources</Link>
-                            <Link className='text-black ms-4 text-decoration-none' to='/contact'>Contact</Link>
-                            <Link className='text-black ms-4 text-decoration-none' to='/about'>About Us</Link>
+                            <Link className='text-white text-decoration-none' to='/resources'>Resources</Link>
+                            <Link className='text-white ms-4 text-decoration-none' to='/contact'>Contact</Link>
+                            <Link className='text-white ms-4 text-decoration-none' to='/about'>About Us</Link>
                        
                      
                     </Nav>
@@ -49,13 +61,11 @@ const Navigation = () => {
                             </Nav.Link> 
                             
                     </Navbar.Text>
-                    
-                    </Navbar.Collapse>
-
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+                </Navbar.Collapse>    
+            </Navbar.Collapse>        
+        </Container>
+    </Navbar>            
+ );
 };
 
 export default Navigation;
