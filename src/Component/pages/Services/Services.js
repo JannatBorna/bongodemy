aa2f44b1e879585554101168d71bf5b5f624803c
Kamrun Nahar <knahar.kamrun@gmail.com>
2022-01-25 22:00:11 +0600

    Services

--- src/Component/pages/Services/Services.js
+++ src/Component/pages/Services/Services.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Services.css'
 
 const Services = () => {
     return (
         <div>
-            <h1>Services</h1>
+            <h1 className='m-5'>Services</h1>
         </div>
     );
 };
