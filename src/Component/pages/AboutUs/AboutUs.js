55530946bbfd237e19c2ab8be973b17962caf9c3
fatehahmod <ahmod.fateh@gmail.com>
2022-02-09 23:53:15 +0600

    added about page

--- src/Component/pages/AboutUs/AboutUs.js
+++ src/Component/pages/AboutUs/AboutUs.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const AboutUs = () => {
     return (
-        <div>
-            <h1>About Page</h1>
+        <div style={{backgroundColor:"#1e2d40"}}>
+           <div className='pt-5 pb-5 ms-5 me-5' >
+            <p className='' style={{backgroundColor:"#0ED7A8" , height:"50px"}}>
+            <Link style={{color:"black", textDecoration:"none"}} to="/overview">Overview</Link></p>
+            <p style={{backgroundColor:"#0ED7A8", height:"50px"}}><Link style={{color:"black", textDecoration:"none"}} to="/overview">Mission</Link></p>
+            <p style={{backgroundColor:"#0ED7A8", height:"50px"}}>
+            <Link style={{color:"black", textDecoration:"none"}} to="/overview">Vision</Link></p>
+            <p style={{backgroundColor:"#0ED7A8", height:"50px"}}>
+            <Link style={{color:"black", textDecoration:"none"}} to="/overview">Team</Link></p>
+        </div>
         </div>
     );
 };
