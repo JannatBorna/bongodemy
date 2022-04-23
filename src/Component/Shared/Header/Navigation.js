08b7a8746bacc3c2f8f2ac35d660fbee0a9b545c
reyad98 <alexmarfi2233@gmail.com>
2022-04-23 06:46:59 +0600

    rey

--- src/Component/Shared/Header/Navigation.js
+++ src/Component/Shared/Header/Navigation.js
@@ -1,16 +1,19 @@
+import { Button } from 'bootstrap';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useAuth from '../../../Hooks/useAuth';
 import logo from '../../../images/icon/BongoDemy-Logo-Transparent.png'
 
 const Navigation = () => {
 
-   
-    
+    const { logOut, user } = useAuth();
+
     return (
         <div >
+            
             <nav style={{ backgroundColor: "#1E2D40" }} className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
-                <img  width="200px" height="50px" src={logo} alt="" className='header_logo pe-3' />
+                    <img width="200px" height="50px" src={logo} alt="" className='header_logo pe-3' />
                     <button className="navbar-toggler text-white bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon "></span>
                     </button>
@@ -19,7 +22,7 @@ const Navigation = () => {
                             <li className="nav-item">
                                 <Link className="nav-link active text-white" aria-current="page" to="/home">Home</Link>
                             </li>
-                            
+
                             <li className="nav-item">
                                 <Link className="nav-link text-white" to="/services">Services</Link>
                             </li>
@@ -40,7 +43,7 @@ const Navigation = () => {
                             <li className="nav-item">
                                 <Link className="nav-link text-white" to='/contact'>Contact</Link>
                             </li>
-                            
+
                             <li className="nav-item dropdown">
                                 <Link className="nav-link dropdown-toggle text-white" to="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     About Us
@@ -51,16 +54,34 @@ const Navigation = () => {
                                     <li><Link className="dropdown-item " to="methodology">Methodology</Link></li>
                                     <li><Link className="dropdown-item " to="standerGuideline">Stander & Guideline</Link></li>
                                     <li><Link className="dropdown-item " to="ourTeamMember">Our Team Members</Link></li>
-                                    
+
                                 </ul>
                             </li>
                         </ul>
                     </div>
                 </div>
                 <div>
-                    <button className='signUp_button m-3'>
-                        <Link className='text-black ms-2 text-decoration-none' to='/login'>Signup/Login</Link>
-                    </button>
+
+
+                    {
+                        user?.email ?
+                            <button className='mt-1 mx-3' onClick={logOut}
+                            style={{ backgroundColor: "Aquamarine", border: 'none', padding: '5px', textAlign: 'center', fontSize: '15px', fontWeight: 'bold', display: 'inline-block', borderRadius: '4px' }}
+
+                            >
+                                <Link className='text-black ms-2 text-decoration-none' to='/login'>Logout</Link>
+                            </button>
+                            :
+                            <button className='mt-1 mx-3'
+                                style={{ backgroundColor: "Aquamarine", border: 'none', padding: '5px', textAlign: 'center', fontSize: '15px', fontWeight: 'bold', display: 'inline-block', borderRadius: '4px' }}
+
+                            >
+                                <Link className='text-black ms-2 text-decoration-none' to='/login'>Signup/Login</Link>
+                            </button>
+                    }
+
+
+
                 </div>
             </nav>
         </div>
